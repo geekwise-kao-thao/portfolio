f04d2a02025adcbd7dbad02f33cdb4af54539ce3
Kao Thao <kausthoj@gmail.com>
2016-06-24 22:45:17 +0000

    layout completed for site

--- js/app.js
+++ js/app.js
@@ -3,10 +3,13 @@ var sticky_header = function (){
     window.addEventListener('scroll',function(event){
         
         var header = document.getElementById('sticky_head');
-        var header_top = header.getBoundingClientRect().top;
-        console.log(header_top);
+        var hero = document.getElementById('header');
         
-        if(header_top <= 1){
+        
+        var hero_top = hero.getBoundingClientRect().top;
+        console.log(hero_top);
+        
+        if(hero_top <= -40){
             header.style.position = 'fixed';
             header.style.top = '0';
             header.style.width = '100%';
