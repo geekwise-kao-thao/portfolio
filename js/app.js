9eff43be1a0312efa3c15599977a93a44188f556
Kao Thao <kausthoj@gmail.com>
2016-06-28 23:31:51 +0000

    parallax mostly completed for hero section

--- js/app.js
+++ js/app.js
@@ -3,15 +3,27 @@ var sticky_header = function (){
     window.addEventListener('scroll',function(event){
         
         var window_top = this.scrollY;
-        var logo_element = document.getElementById('raven_logo');
-        var blurry_bird = document.getElementById('blurry_bird');
+        var logo_element = document.getElementById('name');
+        var blurry_star = document.getElementById('blurry_star');
+        var front_star = document.getElementById('front_star');
         
-        logo_element.style.transform = 'translte(0px,' + window_top /2 + '%)';
-        console.log(logo_element.style.transform = 'translate(0px,' + window_top /2 + '%)');
+        logo_element.style.transform = 'translate(0px,' + window_top /3 + '%)';
+        blurry_star.style.transform = 'translate(0px,' + window_top /5 + '%)';
+        front_star.style.transform = 'translate(0px,-' + window_top /10 + '%)';
         
-        blurry_bird.style.transform = 'translate(-' + window_top /4 + '%,' + window_top /4 + '%)';
-        console.log(blurry_bird.style.transform = 'translate(-' + window_top /4 + '%,' + window_top /4 + '%)');
+        /* ================== nav bar sticky header ================================= */
         
+        var nav_bar = document.getElementById('sticky_head');
+        
+        var nav_bar_top = nav_bar.getBoundingClientRect().top;
+
+        if(nav_bar_top <= 0){
+            nav_bar.style.position = 'fixed';
+            nav_bar.style.top = '0';
+            nav_bar.style.width = '100%';
+            nav_bar.style.backgroundColor = '#636363';
+            //document.getElementById('header').style.marginTop = '7rem';
+        }else{};
         
     });
 };
